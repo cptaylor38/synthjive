879249628684d17cee8ef5a4c49f7e8c9330f594
Corbin Taylor <cptaylor38@yahoo.com>
2021-03-08 12:15:07 -0500

    removed record toggle and created event listeners, may want recording default

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import React, {useState, useEffect} from 'react';
 import { Box, Switch, FormControl, FormLabel } from '@chakra-ui/react';
 import Key from './Key';
 
 function App() {
   const keystrokes = ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'];
+  const [recordedKeys, setRecordedKeys] = useState();
+  const _handleKeyDown = async (e) => {
+    console.log(e);
+  }
+  useEffect(()=> {
+    document.addEventListener('keydown', _handleKeyDown);
+  }, [])
   return (
     <div className="App">
       <div className='synthjive__container'>
@@ -15,17 +23,19 @@ function App() {
         <main className='synth__main'>
           <section>
             <div className='gui__controls'>
-              <Box className="synth__box synth--slider">
+              {/* <Box className="synth__box synth--slider">
                 <FormControl display="flex" justifyContent="center" alignItems="center">
                 <FormLabel className='synth__record--label' htmlFor="record-button" mb="0">
                     Record
                 </FormLabel>
                 <Switch colorScheme="rgb(40, 255, 40);" size="lg" id="record-button" />
                 </FormControl>
-              </Box>
+              </Box> */}
               <Box className="synth__box synth--play">Playback Notes</Box>
               <Box className="synth__box synth--save">Save recording to local storage</Box>
-              <Box className="synth__box synth--notes">Notes/keys being recorded</Box>
+              <Box className="synth__box synth--notes">
+                <p>{}</p>
+              </Box>
             </div>
             <div className='gui__keyboard'>
               <Box className="synth__box synth--keyboard">
