ed79b94706010bde61a1c4a7674122bd2feecdeb
Corbin Taylor <cptaylor38@yahoo.com>
2021-03-10 13:05:55 -0500

    added audio, fixing audio times, bound trigger keys

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,51 @@ import './App.css';
 import React, {useState, useEffect} from 'react';
 import { Box } from '@chakra-ui/react';
 import Key from './Key';
+import a from './Assets/keyboardsounds/a.mp3';
+import s from './Assets/keyboardsounds/s.mp3';
+import d from './Assets/keyboardsounds/d.mp3';
+import f from './Assets/keyboardsounds/f.mp3';
+import g from './Assets/keyboardsounds/g.mp3';
+import h from './Assets/keyboardsounds/h.mp3';
+import j from './Assets/keyboardsounds/j.mp3';
+import k from './Assets/keyboardsounds/k.mp3';
+import l from './Assets/keyboardsounds/l.mp3';
 
 function App() {
-  const keystrokes = ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'];
+  const keystrokes = [ 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'];
   const [recordedKeys, setRecordedKeys] = useState();
   const _handleKeyDown = async (e) => {
-    console.log(e);
+    switch(e.code){
+      case 'KeyA':
+        document.getElementById('note-a').play();
+        break;
+      case 'KeyS':
+        document.getElementById('note-s').play();
+        break;
+      case 'KeyD':
+        document.getElementById('note-d').play();
+        break;
+      case 'KeyF':
+        document.getElementById('note-f').play();
+        break;
+      case 'KeyG':
+        document.getElementById('note-g').play();
+        break;
+      case 'KeyH':
+        document.getElementById('note-h').play();
+        break;
+      case 'KeyJ':
+        document.getElementById('note-j').play();
+        break;
+      case 'KeyK':
+        document.getElementById('note-k').play();
+        break;
+      case 'KeyL':
+        document.getElementById('note-l').play();
+        break;
+      default:
+        return; 
+    }
   }
   useEffect(()=> {
     document.addEventListener('keydown', _handleKeyDown);
@@ -23,14 +62,6 @@ function App() {
         <main className='synth__main'>
           <section>
             <div className='gui__controls'>
-              {/* <Box className="synth__box synth--slider">
-                <FormControl display="flex" justifyContent="center" alignItems="center">
-                <FormLabel className='synth__record--label' htmlFor="record-button" mb="0">
-                    Record
-                </FormLabel>
-                <Switch colorScheme="rgb(40, 255, 40);" size="lg" id="record-button" />
-                </FormControl>
-              </Box> */}
               <Box className="synth__box synth--play">Playback Notes</Box>
               <Box className="synth__box synth--save">Save recording to local storage</Box>
               <Box className="synth__box synth--notes">
@@ -39,9 +70,18 @@ function App() {
             </div>
             <div className='gui__keyboard'>
               <Box className="synth__box synth--keyboard">
-                {keystrokes.map((note, i)=> <Key key={i} text={note}/>)}
+                {keystrokes.map((note, i)=> <Key key={i} text={note} />)}
               </Box>
             </div>
+            <audio id='note-a'><source src={a}></source></audio>
+            <audio id='note-s'><source src={s}></source></audio>
+            <audio id='note-d'><source src={d}></source></audio>
+            <audio id='note-f'><source src={f}></source></audio>
+            <audio id='note-g'><source src={g}></source></audio>
+            <audio id='note-h'><source src={h}></source></audio>
+            <audio id='note-j'><source src={j}></source></audio>
+            <audio id='note-k'><source src={k}></source></audio>
+            <audio id='note-l'><source src={l}></source></audio>
           </section>
           <aside>
             <Box className="synth__box synth--savedheader">Saved songs</Box>
--- client/src/Key.js
+++ client/src/Key.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import './Key.css';
 
-const Key = ({text }) => {
-    
+const Key = ({text, src}) => {
     return (
         <div className='keyboard__key'>
             <div className='keybaord__key--black'></div>
-            <audio autoPlay={true}>
-                <source src={require(`./Assets/keyboardsounds/${text}.mp3`)} type='audio/mp3' />
-            </audio>
             <p>{text}</p>
         </div>
     )
